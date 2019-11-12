c1cbadfe7289fca050796d193de86d804db26079
Reherc <amaury.vanoorenberghe@yahoo.com>
2019-11-12 19:29:28 +0100

    Ajoutez des fichiers projet.

---
+++ Centrifuge.GameSupport.Distance/GameAPI.cs
@@ -0,0 +1,14 @@
+﻿using Reactor.API.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Centrifuge.Distance
+{
+    [GameSupportLibraryEntryPoint(DistanceGameNamespace)]
+    internal sealed class GameAPI
+    {
+        internal const string DistanceGameNamespace = "com.github.reherc/Centrifuge.Distance";
+    }
+}
