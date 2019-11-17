d58b0339a367f5bcb3a07a7b1148b2726ff30f84
Reherc <amaury.vanoorenberghe@yahoo.com>
2019-11-17 02:26:17 +0100

    V1.0.7

--- publish-package.cmd
+++ publish-package.cmd
@@ -1,4 +1,5 @@
 @echo off
+rem this script doesn't have the api key/nuget token for obvious security reasons
 pushd %~dp0
 for /f %%A in ('dir /b /s "Build\nuget\*.nupkg"') do (
     nuget push %%A -Source https://api.nuget.org/v3/index.json
