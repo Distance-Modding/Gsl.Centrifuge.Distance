35353b8a365d9793df138836e76e42eae0b18f0b
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-06-01 00:34:28 +0200

    Fixed broken spectrum dependencies

--- Centrifuge.GameSupport.Distance/Systems/GUI/Data/MenuDisplayMode.cs
+++ Centrifuge.GameSupport.Distance/Systems/GUI/Data/MenuDisplayMode.cs
@@ -1,27 +1,13 @@
 ﻿using System;
-using DisplayMode = global::Spectrum.API.GUI.Data.MenuDisplayMode;
 
 namespace Centrifuge.Distance.GUI.Data
 {
     [Flags]
     public enum MenuDisplayMode
     {
-        None = DisplayMode.None,
-        MainMenu = DisplayMode.MainMenu,
-        PauseMenu = DisplayMode.PauseMenu,
-        Both = DisplayMode.Both
-    }
-
-    public static class MenuDisplayModeEx
-    {
-        public static MenuDisplayMode Mode(this DisplayMode mode)
-        {
-            return (MenuDisplayMode)mode;
-        }
-
-        public static DisplayMode Mode(this MenuDisplayMode mode)
-        {
-            return (DisplayMode)mode;
-        }
+        None,
+        MainMenu,
+        PauseMenu,
+        Both
     }
 }
