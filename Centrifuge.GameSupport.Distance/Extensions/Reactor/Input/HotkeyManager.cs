388f3521c6010da9ffd2328767f25b0f58e5e43e
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-08-04 00:33:57 +0200

    Added HotkeyManager extension methods

---
+++ Centrifuge.GameSupport.Distance/Extensions/Reactor/Input/HotkeyManager.cs
@@ -0,0 +1,23 @@
+﻿using Reactor.API.Input;
+using Reactor.Input;
+using System;
+
+public static class HotkeyManagerExtensions
+{
+    public static Hotkey BindHotkey(this HotkeyManager m, string hotkeyString, Action action, bool isOneTime = false)
+    {
+        Hotkey hotkey = new Hotkey(hotkeyString, isOneTime);
+
+        m.Bind(hotkey, action);
+
+        return hotkey;
+    }
+
+    public static void UnbindHotkey(this HotkeyManager m, Hotkey hotkey)
+    {
+        if (m.ActionHotkeys.ContainsKey(hotkey))
+        {
+            m.ActionHotkeys.Remove(hotkey);
+        }
+    }
+}
