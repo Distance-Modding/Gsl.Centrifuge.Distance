d70801c9b768c20f2f2a69e88b6f9edcac886ff6
Reherc <amaury.vanoorenberghe@yahoo.com>
2020-08-16 20:01:21 +0200

    Added quality of life patches

---
+++ Centrifuge.GameSupport.Distance/Systems/EditorInspector/Harmony/Assembly-CSharp/NGUIObjectInspectorTabAbstract/CreateComponentInspectorsOnObject.cs
@@ -0,0 +1,20 @@
+﻿using Harmony;
+using UnityEngine;
+
+namespace Centrifuge.Distance.Systems.EditorInspector.Harmony
+{
+    [HarmonyPatch(typeof(NGUIObjectInspectorTabAbstract), "CreateComponentInspectorsOnObject")]
+    internal class NGUIObjectInspectorTabAbstract__CreateComponentInspectorsOnObject
+    {
+        [HarmonyPrefix]
+        internal static void Prefix(GameObject obj)
+        {
+            Group group = obj.GetComponent<Group>();
+
+            if (group)
+            {
+                group.inspectChildren_ = Group.InspectChildrenType.None;
+            }
+        }
+    }
+}
---
+++ Centrifuge.GameSupport.Distance/Systems/EditorInspector/Harmony/Assembly-CSharp/NGUIObjectInspectorTabAbstract/CreateISerializableInspector.cs
@@ -0,0 +1,23 @@
+﻿using Harmony;
+using System;
+using System.Linq;
+
+namespace Centrifuge.Distance.Systems.EditorInspector.Harmony
+{
+    [HarmonyPatch(typeof(NGUIObjectInspectorTabAbstract), "CreateISerializableInspector", new Type[2] { typeof(ISerializable), typeof(bool) })]
+    internal class NGUIObjectInspectorTabAbstract__CreateISerializableInspector
+    {
+        [HarmonyPrefix]
+        internal static bool Prefix(NGUIObjectInspectorTabAbstract __instance, ISerializable serializable)
+        {
+            if (serializable is Group group && !group.gameObject.GetChildren().Any())
+            {
+                group.inspectChildren_ = Group.InspectChildrenType.None;
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
