1a8175157aa1baed81284e96d63e5bf5566df4ea
REHERC <amaury.vanoorenberghe@yahoo.com>
2022-02-22 04:43:22 +0100

    Removed nuget binary and old publish script

--- publish-package.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-rem this script doesn't have the api key/nuget token for obvious security reasons
-pushd %~dp0
-for /f %%A in ('dir /b /s "Build\nuget\*.nupkg"') do (
-    nuget push %%A -Source https://api.nuget.org/v3/index.json
-)
-popd
-pause > nul
